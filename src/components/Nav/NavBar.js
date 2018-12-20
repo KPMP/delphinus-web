9cfddb7421027d7d6462a27198f59e35091c3267
Zach Wright <zwright@umich.edu>
2018-12-20 12:38:18 -0500

    KPMP-591: Text changes

--- src/components/Nav/NavBar.js
+++ src/components/Nav/NavBar.js
@@ -10,8 +10,9 @@ class NavBar extends Component {
 						<img src="img/logo.png" alt="Digital Pathology Repository" className="logo"/>
 					</NavbarBrand>
 				</Col>
-				<Col xs="6" className="demo-text">Slide Viewer Concept
+				<Col xs="6" id="demo-text">Slide Viewer Concept
 				</Col>
+				<Col xs="6" id="demo-text-small"></Col>
 				<Col xs="3">
 					<div className="float-right" id="feedback-button"><Button color="primary" onClick={() => window.open("", "_blank")}>Send Feedback</Button></div>
 				</Col>
