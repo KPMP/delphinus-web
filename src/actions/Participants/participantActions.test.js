3cbf96bac7d0a746f253c94294924a9aa16643a4
Zach Wright <zwright@umich.edu>
2019-08-26 11:33:17 -0400

    KPMP-1222: Add test

--- src/actions/Participants/participantActions.test.js
+++ src/actions/Participants/participantActions.test.js
@@ -1,4 +1,4 @@
-import { setSelectedParticipant } from './participantActions';
+import { setSelectedParticipant, setParticipants } from './participantActions';
 import actionNames from '../actionNames';
 
 
@@ -9,6 +9,17 @@ describe('setSelectedParticipant', () => {
 		
 		let result = setSelectedParticipant(payload);
 		
+		expect(result).toEqual( { payload: payload, type: actionName });
+	});
+});
+
+describe('setParticipants', () => {
+	it('should pass the argument through to the payload and set the action', () => {
+		let payload = "I am a payload";
+		let actionName = actionNames.SET_PARTICIPANTS;
+
+		let result = setParticipants(payload);
+
 		expect(result).toEqual( { payload: payload, type: actionName });
 	});
 });
