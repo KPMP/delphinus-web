9c431e5bf1c341430ba65bf0531ba86f6d898025
Becky Reamy <rlreamy@umich.edu>
2019-11-08 12:56:03 -0500

    KPMP-1416: Add permission denied pages

--- src/components/Error/NotRegistered.js
+++ src/components/Error/NotRegistered.js
@@ -5,17 +5,17 @@ class NotRegistered extends Component {
 	
 	render() {
 		return (
-			<article className="container justify-content-center pt-3">
+			<article id='permissions' className='container justify-content-center pt-3'>
 				<Row >
-					<Col xs={12} className="error-header pb-3">
+					<Col xs={12} className='error-header pb-3'>
 						We're sorry. You are not authorized to access this application.
 					</Col>
 				</Row>
 				<Row>
 					<Col xs={12}>
-						<div  className="alert alert-danger">
+						<div  className='alert alert-danger'>
 						You must be a registered user in order to use this KPMP application. If you would like to register or believe you received this page in error, please contact: &nbsp;
-							<a href="mailto:admin@kpmp.org">admin@kpmp.org</a>
+							<a href='mailto:admin@kpmp.org'>admin@kpmp.org</a>
 						</div>
 					</Col>
 				</Row>
--- src/components/Error/PermissionDenied.js
+++ src/components/Error/PermissionDenied.js
@@ -5,17 +5,17 @@ class PermissionDenied extends Component {
 	
 	render() {
 		return (
-			<article className="container justify-content-center pt-3">
+			<article id='permissions' className='container justify-content-center pt-3'>
 				<Row >
-					<Col xs={12} className="error-header pb-3">
+					<Col xs={12} className='error-header pb-3'>
 						We're sorry. You are not authorized to access this application.
 					</Col>
 				</Row>
 				<Row>
 					<Col xs={12}>
-						<div  className="alert alert-danger">
+						<div  className='alert alert-danger'>
 						Your account does not have permission to use this KPMP application. If you believe you received this page in error, please contact: &nbsp;
-							<a href="mailto:admin@kpmp.org">admin@kpmp.org</a>
+							<a href='mailto:admin@kpmp.org'>admin@kpmp.org</a>
 						</div>
 					</Col>
 				</Row>
