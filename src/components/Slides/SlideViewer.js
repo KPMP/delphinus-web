4aad1deb3ba0cec3420212bd9416a4c7e3a173c8
Alex Barkell <albarkel@umich.edu>
2021-06-15 17:37:16 -0400

    add simple grid to slide viewer

--- src/components/Slides/SlideViewer.js
+++ src/components/Slides/SlideViewer.js
@@ -8,11 +8,43 @@ import PropTypes from 'prop-types';
 
 class SlideViewer extends Component {
 
+	getGridOverlay(metadata) {
+		let overlay = []
+		if (metadata && metadata.aperio && metadata.aperio.originalHeight && metadata.aperio.originalWidth) {
+
+			let width = parseInt(metadata.aperio.originalWidth);
+			let height = parseInt(metadata.aperio.originalHeight);
+
+			for (let i = 0; i < width; i += 1000) {
+				overlay.push({
+					px: i,
+					py: 0,
+					width: 100,
+					height: height,
+					className: 'hightlight'
+				})
+			}
+			for (let i = 0; i < height; i += 1000) {
+				overlay.push({
+					px: 0,
+					py: i,
+					width: width,
+					height: 100,
+					className: 'hightlight'
+				})
+			}
+		} else {
+			console.error('Metadata not provided with slide')
+		}
+		return overlay;
+	}
+
 	initSeaDragon() {
 		let self = this;
 		let slideId = this.props.selectedParticipant.selectedSlide.id;
-		OpenSeadragon.setString("Tooltips.Home","Reset pan & zoom");
-		self.viewer =  OpenSeadragon({
+		let gridOverlay = this.getGridOverlay(this.props.selectedParticipant.selectedSlide.metadata)
+		OpenSeadragon.setString("Tooltips.Home", "Reset pan & zoom");
+		self.viewer = OpenSeadragon({
 			id: "osdId",
 			visibilityRatio: 0.5,
 			constrainDuringPan: false,
@@ -26,20 +58,22 @@ class SlideViewer extends Component {
 			nextButton: 'next',
 			previousButton: 'previous',
 			showNavigator: true,
-			navigatorAutoFade:  false,
-			navigatorId:   'osd-navigator',
-			tileSources: 'deepZoomImages/' + slideId + '.dzi'
+			navigatorAutoFade: false,
+			navigatorId: 'osd-navigator',
+			tileSources: 'deepZoomImages/' + slideId + '.dzi',
+			overlays: gridOverlay
+
 		});
 	}
 
-	componentDidMount(){
-		if(!noSlidesFound(this.props.selectedParticipant, this.props.handleError)) {
-            this.initSeaDragon();
+	componentDidMount() {
+		if (!noSlidesFound(this.props.selectedParticipant, this.props.handleError)) {
+			this.initSeaDragon();
 		}
 		document.body.classList.add('slide-viewer-body');
 	}
 
-	componentDidUpdate(){
+	componentDidUpdate() {
 		this.viewer.destroy();
 		this.viewer.navigator.destroy();
 		this.initSeaDragon();
@@ -49,8 +83,12 @@ class SlideViewer extends Component {
 	render() {
 		return (
 			<div id="slide-viewer" className="container-fluid">
-				<Menu selectedParticipant={this.props.selectedParticipant}/>
-				<div className="osd-div" ref={node => {this.el = node;}}>
+				<Menu selectedParticipant={this.props.selectedParticipant} />
+				<div id="osdOverlay">
+
+				</div>
+				<hr />
+				<div className="osd-div" ref={node => { this.el = node; }}>
 					<div className="openseadragon" id="osdId"></div>
 					<ul className="osd-toolbar">
 						<li><div className="osd-button" id="zoom-in"><FontAwesomeIcon icon={faPlus} /></div></li>
