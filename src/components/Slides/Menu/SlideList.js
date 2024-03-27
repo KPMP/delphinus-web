a059f9cd699f1c8fc14100a0e6bcf3f145228def
Nathan Creger <nathancreger1111@gmail.com>
2024-03-27 16:16:05 -0400

    use accordionlist container

--- src/components/Slides/Menu/SlideList.js
+++ src/components/Slides/Menu/SlideList.js
@@ -3,11 +3,12 @@ import { Col, UncontrolledAccordion} from 'reactstrap';
 import {
 	noSlidesFound,
 } from '../slideHelpers.js';
-import AccordionList from './AccordionList.js';
+// import AccordionList from './AccordionList.js';
 import PropTypes from 'prop-types';
 
 
 import Header from './Header';
+import AccordionListContainer from './AccordionListContainer.js';
 
 class SlideList extends Component {
 
@@ -22,9 +23,9 @@ class SlideList extends Component {
 				<Header {...this.props} />
 				<Col id="slides-col">
           <UncontrolledAccordion defaultOpen={['1', '2', '3']} stayOpen >
-            {slideType === "(LM) Light Microscopy" ? <AccordionList selectedParticipant={this.props.selectedParticipant} slideType={slideType} accordionId='1' targetId='1' /> : ''}
-            {slideType === "(EM) Electron Microscopy" ? <AccordionList selectedParticipant={this.props.selectedParticipant} slideType={slideType} accordionId='2' targetId='2' /> : ''}
-            {slideType === "(IF) Immunoflourescence" ? <AccordionList selectedParticipant={this.props.selectedParticipant} slideType={slideType} accordionId='3' targetId='3' /> : ''}
+            {slideType === "(LM) Light Microscopy" ? <AccordionListContainer selectedParticipant={this.props.selectedParticipant} slideType={slideType} accordionId='1' targetId='1' /> : ''}
+            {slideType === "(EM) Electron Microscopy" ? <AccordionListContainer selectedParticipant={this.props.selectedParticipant} slideType={slideType} accordionId='2' targetId='2' /> : ''}
+            {slideType === "(IF) Immunoflourescence" ? <AccordionListContainer selectedParticipant={this.props.selectedParticipant} slideType={slideType} accordionId='3' targetId='3' /> : ''}
    
           </UncontrolledAccordion>
 				</Col>
