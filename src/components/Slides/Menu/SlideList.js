c027cbf6e10e1293305d4aca64c82b7617ff5673
Nathan Creger <nathancreger1111@gmail.com>
2024-03-28 09:33:59 -0400

    try async accordion loading

--- src/components/Slides/Menu/SlideList.js
+++ src/components/Slides/Menu/SlideList.js
@@ -11,9 +11,27 @@ import AccordionListContainer from './AccordionListContainer.js';
 
 class SlideList extends Component {
 
+  constructor(props){
+    super(props)
+    this.state({loaded: false})
+  }
+
 	componentDidUpdate() {
 		noSlidesFound(this.props.selectedParticipant);
 	}
+
+  content(slideType) {
+      <UncontrolledAccordion defaultOpen={['1', '2', '3']} stayOpen >
+        {slideType === "(LM) Light Microscopy" ? <AccordionListContainer toggleMenu={this.props.toggleMenu} selectedParticipant={this.props.selectedParticipant} slideType={slideType} accordionId='1' targetId='1' /> : null}
+        {slideType === "(EM) Electron Microscopy" ? <AccordionListContainer toggleMenu={this.props.toggleMenu} selectedParticipant={this.props.selectedParticipant} slideType={slideType} accordionId='2' targetId='2' /> : null}
+        {slideType === "(IF) Immunoflourescence" ? <AccordionListContainer toggleMenu={this.props.toggleMenu} selectedParticipant={this.props.selectedParticipant} slideType={slideType} accordionId='3' targetId='3' /> : null}
+      </UncontrolledAccordion>
+  }
+
+  async componentDidMount() {
+    await this.props.setSelectedPaticipant(this.props.selectedParticipant)
+    this.setState({loaded: false})
+  }
 
 	render() {
 		let slideType = this.props.selectedParticipant.selectedSlide.slideType;
@@ -21,12 +39,7 @@ class SlideList extends Component {
 			<div id="menu-slide-list">
 				<Header {...this.props} />
 				<Col id="slides-col">
-          <UncontrolledAccordion defaultOpen={['1', '2', '3']} stayOpen >
-            {slideType === "(LM) Light Microscopy" ? <AccordionListContainer toggleMenu={this.props.toggleMenu} selectedParticipant={this.props.selectedParticipant} slideType={slideType} accordionId='1' targetId='1' /> : ''}
-            {slideType === "(EM) Electron Microscopy" ? <AccordionListContainer toggleMenu={this.props.toggleMenu} selectedParticipant={this.props.selectedParticipant} slideType={slideType} accordionId='2' targetId='2' /> : ''}
-            {slideType === "(IF) Immunoflourescence" ? <AccordionListContainer toggleMenu={this.props.toggleMenu} selectedParticipant={this.props.selectedParticipant} slideType={slideType} accordionId='3' targetId='3' /> : ''}
-   
-          </UncontrolledAccordion>
+          {this.state.loaded ? this.content(slideType) : null}
 				</Col>
 			</div>
 		);
