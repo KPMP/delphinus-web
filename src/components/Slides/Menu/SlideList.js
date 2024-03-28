6e1899872701d5dd242180a6d87f2fb08cf16946
Nathan Creger <nathancreger1111@gmail.com>
2024-03-28 12:19:06 -0400

    fix accordions

--- src/components/Slides/Menu/SlideList.js
+++ src/components/Slides/Menu/SlideList.js
@@ -27,7 +27,7 @@ class SlideList extends Component {
 				<Col id="slides-col">
         <UncontrolledAccordion defaultOpen={['1', '2', '3']} stayOpen >
           {
-            this.props.selectedParticipant.slides.map(function (slide, index){
+            Object.keys(this.props.selectedParticipant).map(function (slide, index){
               let slideType = this.props.slideType;
               let toggleMenu = this.props.toggleMenu;
               let selectedParticipant = this.props.selectedParticipant;
