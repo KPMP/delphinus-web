bf7a5cddfed6847bc42a32e564ff9fef3ce5bb57
Nathan Creger <nathancreger1111@gmail.com>
2024-03-28 15:17:35 -0400

    trying dispatch

--- src/actions/Participants/participantActions.js
+++ src/actions/Participants/participantActions.js
@@ -31,24 +31,24 @@ export const getParticipantSlides = (participantId, props) => {
 		axios.get('/api/v1/slides/' + participantId, config)
 			.then(result => {
         let data = Object.keys(result.data)
-        // if (result.data["(LM) Light Microscopy"] != null){
-        //   let slides = participantSelectSorter(result.data["(LM) Light Microscopy"])
-        //   // dispatch(setSelectedParticipant({id: participantId, slides: slides, selectedSlide: slides[0]}));
-        // }
-        // if (result.data["(IF) Immunofluorescence"] != null){
-        //   let slides = participantSelectSorter(result.data["(IF) Immunofluorescence"])
-        //   console.log("append IF")
-        //   // dispatch(setSelectedParticipant({id: participantId, slides: slides, selectedSlide: slides[0]}));
-        // }
-        // if (result.data["(EM) Electron Microscopy"] != null){
-        //   let slides = participantSelectSorter(result.data["(EM) Electron Microscopy"])
-        //   console.log("append EM")
-        //   // dispatch(setSelectedParticipant({id: participantId, slides: slides, selectedSlide: slides[0]}));
-        // }
+        if (result.data["(LM) Light Microscopy"] != null){
+          let slides = participantSelectSorter(result.data["(LM) Light Microscopy"])
+          // dispatch(setSelectedParticipant({id: participantId, slides: slides, selectedSlide: slides[0]}));
+        }
+        if (result.data["(IF) Immunofluorescence"] != null){
+          let slides = participantSelectSorter(result.data["(IF) Immunofluorescence"])
+          console.log("append IF")
+          // dispatch(setSelectedParticipant({id: participantId, slides: slides, selectedSlide: slides[0]}));
+        }
+        if (result.data["(EM) Electron Microscopy"] != null){
+          let slides = participantSelectSorter(result.data["(EM) Electron Microscopy"])
+          console.log("append EM")
+          // dispatch(setSelectedParticipant({id: participantId, slides: slides, selectedSlide: slides[0]}));
+        }
 
-        for (let i = 0; i < data.length(); i ++ ){
+        for (let i = 0; i < data.length; i++){
           console.log(data[i]);
-          // dispatch(setSelectedParticipant({id: participantId, slide: data[i], selectedSlide:data[i]}));
+          dispatch(setSelectedParticipant({id: participantId, slide: data[i], selectedSlide:data[i]}));
         }
         // dispatch(setSelectedParticipant({id: participantId, slides: data[index], selectedSlide: data[0]}));
 				props.history.push(process.env.PUBLIC_URL + "/slides");
