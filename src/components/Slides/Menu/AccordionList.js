23698d8b22b11c1914d42af6ce8b28251fa1e4af
Nathan Creger <nathancreger1111@gmail.com>
2024-03-29 10:17:10 -0400

    iterate on slidetype

--- src/components/Slides/Menu/AccordionList.js
+++ src/components/Slides/Menu/AccordionList.js
@@ -18,7 +18,6 @@ class AccordionList extends Component {
 	}
 
   render() {
-    console.log(this.props.slideType)
     return (
       <AccordionItem>
               <AccordionHeader targetId={this.props.targetId}>
@@ -27,7 +26,7 @@ class AccordionList extends Component {
               <AccordionBody accordionId={this.props.accordionId}>
               <div id="menu-slide-list-slides">
                 {
-                  this.props.slideType.map(function (slide, index) {
+                  this.props.selectedParticipant.slides[this.props.slideType].map(function (slide, index) {
                     let highlightedClass = this.props.selectedParticipant.selectedSlide.id === slide.id ? " slide-highlighted" : "";
                     let thumbnailSrc = "img/thumbnail_stain_" + getStainImageName(slide.stain.type) + ".png";
                     return (
