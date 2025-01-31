f1b9850b7dffdce4bb71cbe797b21b95eee59b3a
dert1129 <nathancreger1111@gmail.com>
2025-01-31 13:55:20 -0500

    log result

--- src/actions/Participants/participantActions.js
+++ src/actions/Participants/participantActions.js
@@ -86,6 +86,7 @@ export const getMetadataForSlide = (participantId, slideName) => {
         var config = { headers: {'Content-Type': 'application/json', 'Cache-control': 'no-cache'}};
         try {
             const result = axios.get(`/api/v1/metadata/${participantId}/${slideName}`, config);
+            console.log(result);
             let metadata = result.data;
             dispatch(setSelectedMetadata(metadata));
             return metadata;
