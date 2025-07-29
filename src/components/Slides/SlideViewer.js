d50e9cf9d4b941e614564344707c5b1f5ab436d5
dert1129 <nathancreger1111@gmail.com>
2025-07-29 11:26:14 -0400

    cleanup after ourselves in our slide viewer

--- src/components/Slides/SlideViewer.js
+++ src/components/Slides/SlideViewer.js
@@ -8,160 +8,176 @@ import PropTypes from 'prop-types';
 import DivOverlays from './DivOverlays';
 
 class SlideViewer extends Component {
-	constructor(props) {
-		super(props);
-		if (!this.props.selectedParticipant || this.props.selectedParticipant.id === "") {
-			window.location.href = "/";
-		}
+  constructor(props) {
+    super(props);
 
-		this.horizontalRef = React.createRef(500);
-		this.verticalRef = React.createRef(500);
-		this.handleShowGridToggle = this.handleShowGridToggle.bind(this)
-		this.handleShowLabelToggle = this.handleShowLabelToggle.bind(this)
-		this.handleCancelGridPropertiesClick = this.handleCancelGridPropertiesClick.bind(this);
+    if (!props.selectedParticipant || props.selectedParticipant.id === "") {
+      window.location.href = "/";
+    }
 
-		this.state = {
-			showGrid: false,
-			showGridLabel: false,
-			overlayDivs: '',
-			overlayLabel: [],
-			renderLabels: true,
-			gridOverlay: null,
+    this.horizontalRef = React.createRef(500);
+    this.verticalRef = React.createRef(500);
+
+    this.handleShowGridToggle = this.handleShowGridToggle.bind(this);
+    this.handleShowLabelToggle = this.handleShowLabelToggle.bind(this);
+    this.handleCancelGridPropertiesClick = this.handleCancelGridPropertiesClick.bind(this);
+
+    this.state = {
+      showGrid: false,
+      showGridLabel: false,
+      overlayDivs: '',
+      overlayLabel: [],
+      renderLabels: false,
+      gridOverlay: null,
       loaded: false,
-		}
-	}
+    };
 
-	async componentDidMount() {
-		await this.props.selectedParticipant.selectedSlide.slideType
-		
-			if (!noSlidesFound(this.props.selectedParticipant, this.props.handleError)) {
-				await this.renderOverlayLabels();
-				this.initSeaDragon();
-			}
-		this.setState({loaded: true})
-	}
+    this.viewer = null;
+  }
 
-	async componentDidUpdate(prevProps, prevState) {
-		if (prevProps.selectedParticipant !== this.props.selectedParticipant) {
-			this.viewer.destroy();
-			this.viewer.navigator.destroy();
-			noSlidesFound(this.props.selectedParticipant, this.props.handleError);
-			await this.renderOverlayLabels();
-			this.initSeaDragon();
-		}
-	}
+  async componentDidMount() {
+    if (!noSlidesFound(this.props.selectedParticipant, this.props.handleError)) {
+      this.renderOverlayLabels();
+      this.initSeaDragon();
+    }
+    this.setState({ loaded: true });
+  }
 
-	async renderOverlayLabels() {
-		if(this.props.selectedParticipant.selectedSlide.slideType === "(LM) Light Microscopy" &&
-			!(this.props.selectedParticipant.selectedSlide?.removed === true)){
-			await this.setState({
-				overlayLabel: this.props.selectedParticipant.selectedSlide.metadata.overlayLabel,
-				gridOverlay: this.props.selectedParticipant.selectedSlide.metadata.overlay,
-				renderLabels: false,
-				}
-			)
-			await this.setState({renderLabels: true});
-		}
-		else {
-			await this.setState({
-				overlayLabel: [],
-				gridOverlay: null,
-				renderLabels: false,
-			})
-		}
-	}
+  async componentDidUpdate(prevProps) {
+    // Only re-init if the slide actually changed
+    const prevSlideId = prevProps.selectedParticipant?.selectedSlide?.id;
+    const currentSlideId = this.props.selectedParticipant?.selectedSlide?.id;
 
-	initSeaDragon() {
-		let slideId = this.props.selectedParticipant.selectedSlide.id;
+    if (prevSlideId !== currentSlideId) {
+      this.destroyViewer();
+      noSlidesFound(this.props.selectedParticipant, this.props.handleError);
+      this.renderOverlayLabels();
+      this.initSeaDragon();
+    }
+  }
 
-		OpenSeadragon.setString("Tooltips.Home", "Reset pan & zoom");
-		this.viewer = OpenSeadragon({
-			id: "osdId",
-			visibilityRatio: 0.5,
-			constrainDuringPan: false,
-			defaultZoomLevel: 1,
-			minZoomLevel: 0.5,
-			maxZoomLevel: 120,
-			zoomInButton: 'zoom-in',
-			zoomOutButton: 'zoom-out',
-			homeButton: 'reset',
-			fullPageButton: 'full-page',
-			nextButton: 'next',
-			previousButton: 'previous',
-			showNavigator: true,
-			navigatorAutoFade: false,
-			navigatorId: 'osd-navigator',
-			tileSources: 'deepZoomImages/' + slideId + '.dzi',
-			overlays: this.state.gridOverlay
-		});
-	}
+  componentWillUnmount() {
+    this.destroyViewer();
+  }
 
-	handleShowGridToggle() {
-		if (this.state.showGrid) {
-			this.setState({ showGrid: false, showGridLabel: false })
-		} else {
-			this.setState({ showGrid: true })
-		}
-	}
+  destroyViewer() {
+    if (this.viewer) {
+      this.viewer.destroy();
+      this.viewer = null;
+    }
+  }
 
-	handleShowLabelToggle() {
-		if (this.state.showGridLabel) {
-			this.setState({ showGridLabel: false })
-		} else {
-			this.setState({ showGrid: true, showGridLabel: true })
-		}
-	}
+  renderOverlayLabels() {
+    const slide = this.props.selectedParticipant.selectedSlide;
 
-	handleCancelGridPropertiesClick(showGridLabel) {
-		this.setState({ showGridLabel })
-	}
+    if (slide.slideType === "(LM) Light Microscopy" && !slide.removed) {
+      this.setState({
+        overlayLabel: slide.metadata.overlayLabel,
+        gridOverlay: slide.metadata.overlay,
+        renderLabels: true,
+      });
+    } else {
+      this.setState({
+        overlayLabel: [],
+        gridOverlay: null,
+        renderLabels: false,
+      });
+    }
+  }
 
-	render() {
-		return (
-			<div>
-				{(this.state.overlayLabel.length >= 1 && this.state.renderLabels) &&
-					<DivOverlays showGridLabel={this.state.showGridLabel} overlayLabels={this.state.overlayLabel} />
-				}
-				<div id="slide-viewer" className="container-fluid">
-        
-        {
-          this.state.loaded ? 
-          <Menu
-						handleShowGridToggle={this.handleShowGridToggle}
-						handleShowLabelToggle={this.handleShowLabelToggle}
-						handleCancelGridPropertiesClick={this.handleCancelGridPropertiesClick}
-						showGrid={this.state.showGrid}
-						showGridLabel={this.state.showGridLabel}
-						vertical='500'
-						horizontal='500'
-						horizontalRef={this.horizontalRef}
-						verticalRef={this.verticalRef}
-						selectedParticipant={this.props.selectedParticipant}/>
-            :
-            null
-        }
-					
+  initSeaDragon() {
+    const slideId = this.props.selectedParticipant.selectedSlide.id;
 
-					<div className="osd-div" ref={node => { this.el = node; }}>
-						<div className={`openseadragon ${(this.state.showGrid) ? 'showGridlines' : 'hideGridlines'}`} id="osdId"></div>
-						<ul className="osd-toolbar">
-							<li><div className="osd-button" id="zoom-in"><FontAwesomeIcon icon={faPlus} /></div></li>
-							<li><div className="osd-button" id="zoom-out"><FontAwesomeIcon icon={faMinus} /></div></li>
-							<li><div className="osd-button" id="reset"><FontAwesomeIcon icon={faCrosshairs} /></div></li>
-						</ul>
-						<div className="osd-navigator-wrapper">
-							<div id="osd-navigator"></div>
-						</div>
-					</div>
-				</div>
-			</div>
-		)
-	}
+    OpenSeadragon.setString("Tooltips.Home", "Reset pan & zoom");
+
+    this.viewer = OpenSeadragon({
+      id: "osdId",
+      visibilityRatio: 0.5,
+      constrainDuringPan: false,
+      defaultZoomLevel: 1,
+      minZoomLevel: 0.5,
+      maxZoomLevel: 40, // Lowered from 120 for performance
+      zoomInButton: 'zoom-in',
+      zoomOutButton: 'zoom-out',
+      homeButton: 'reset',
+      fullPageButton: 'full-page',
+      nextButton: 'next',
+      previousButton: 'previous',
+      showNavigator: true,
+      navigatorAutoFade: false,
+      navigatorId: 'osd-navigator',
+      tileSources: 'deepZoomImages/' + slideId + '.dzi',
+      overlays: this.state.gridOverlay
+    });
+  }
+
+  handleShowGridToggle() {
+    this.setState((prev) => ({
+      showGrid: !prev.showGrid,
+      showGridLabel: prev.showGrid ? false : prev.showGridLabel
+    }));
+  }
+
+  handleShowLabelToggle() {
+    this.setState((prev) => ({
+      showGrid: true,
+      showGridLabel: !prev.showGridLabel
+    }));
+  }
+
+  handleCancelGridPropertiesClick(showGridLabel) {
+    this.setState({ showGridLabel });
+  }
+
+  render() {
+    return (
+      <div>
+        {(this.state.overlayLabel.length >= 1 && this.state.renderLabels) && (
+          <DivOverlays
+            showGridLabel={this.state.showGridLabel}
+            overlayLabels={this.state.overlayLabel}
+          />
+        )}
+
+        <div id="slide-viewer" className="container-fluid">
+          {this.state.loaded && (
+            <Menu
+              handleShowGridToggle={this.handleShowGridToggle}
+              handleShowLabelToggle={this.handleShowLabelToggle}
+              handleCancelGridPropertiesClick={this.handleCancelGridPropertiesClick}
+              showGrid={this.state.showGrid}
+              showGridLabel={this.state.showGridLabel}
+              vertical="500"
+              horizontal="500"
+              horizontalRef={this.horizontalRef}
+              verticalRef={this.verticalRef}
+              selectedParticipant={this.props.selectedParticipant}
+            />
+          )}
+
+          <div className="osd-div" ref={(node) => { this.el = node; }}>
+            <div
+              className={`openseadragon ${this.state.showGrid ? 'showGridlines' : 'hideGridlines'}`}
+              id="osdId"
+            ></div>
+            <ul className="osd-toolbar">
+              <li><div className="osd-button" id="zoom-in"><FontAwesomeIcon icon={faPlus} /></div></li>
+              <li><div className="osd-button" id="zoom-out"><FontAwesomeIcon icon={faMinus} /></div></li>
+              <li><div className="osd-button" id="reset"><FontAwesomeIcon icon={faCrosshairs} /></div></li>
+            </ul>
+            <div className="osd-navigator-wrapper">
+              <div id="osd-navigator"></div>
+            </div>
+          </div>
+        </div>
+      </div>
+    );
+  }
 }
 
 SlideViewer.propTypes = {
-	selectedParticipant: PropTypes.object.isRequired,
-	handleError: PropTypes.func.isRequired
+  selectedParticipant: PropTypes.object.isRequired,
+  handleError: PropTypes.func.isRequired
 };
 
 export default SlideViewer;
