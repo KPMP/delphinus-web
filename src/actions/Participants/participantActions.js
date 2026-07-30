0638067e8c58343a87690bbf4fb0efc95d600df7
dert1129 <nathancreger1111@gmail.com>
2026-07-30 11:43:47 -0400

    fix dispatch

--- src/actions/Participants/participantActions.js
+++ src/actions/Participants/participantActions.js
@@ -75,12 +75,14 @@ export const getAllParticipants = () => {
 }
 
 export const getSlideMetadata = (participantId, slideName) => {
-    var config = {headers: {'Content-Type': "application/json", 'Cache-control': 'no-cache'}}
-    axios.get('api/v1/metadata/' + participantId + "/" + slideName)
-        .then(result => {
-            console.log(result);
-        }).catch(error => {
-            console.log("There was an error getting the metadata for slide " + slideName + " and participant ID" + participantId);
-            dispatch(sendMessageToBackend(error))
-        })
+    return (dispatch) => {
+        var config = {headers: {'Content-Type': "application/json", 'Cache-control': 'no-cache'}}
+        axios.get('api/v1/metadata/' + participantId + "/" + slideName)
+            .then(result => {
+                console.log(result);
+            }).catch(error => {
+                console.log("There was an error getting the metadata for slide " + slideName + " and participant ID" + participantId);
+                dispatch(sendMessageToBackend(error))
+            })
+    }
 }
